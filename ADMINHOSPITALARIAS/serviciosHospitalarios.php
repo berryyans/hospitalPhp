035d4103aa3e1a25601509f957800a5e3b8bc632
EDER MARTINEZ <edermtz.04@gmail.com>
2013-06-24 13:03:38 -0500

    terminado AdminHospitalaria/servicios/catalogo

--- ADMINHOSPITALARIAS/serviciosHospitalarios.php
+++ ADMINHOSPITALARIAS/serviciosHospitalarios.php
@@ -1,6 +1,17 @@
-<?PHP require("menuOperaciones.php"); ?>
+<?PHP 
+//require("menuOperaciones.php"); 
+require("/configuracion/ventanasEmergentes.php");
+require('/configuracion/funciones.php');
+
+$mostrarmenu=new menus();
+$mostrarmenu->menuTemplate($_GET['warehouse'],$_GET['datawarehouse'],$rutasalir,$rutapasswd,$usuario,$entidad,$rutamenuprincipal,'principal',$rutaimagen,$basedatos);
+
+?>
 <?php require('/configuracion/clases/catalogoServiciosHospitalarios.php'); ?>
 <?php
 $catalogoServiciosxInterp=new  catalogosS();
 $catalogoServiciosxInterp->catalogosServicios($entidad,$almacenSolicitante,$usuario,$fecha1,$basedatos);
+
+$mostrarFooter=new menus();
+$mostrarFooter->footerTemplate();
 ?>
