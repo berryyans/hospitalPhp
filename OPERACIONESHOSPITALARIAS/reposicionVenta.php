b8260e9c7fcb76a60f08e1d1cfc3c33edce02783
EDER MARTINEZ <edermtz.04@gmail.com>
2013-06-27 18:08:37 -0500

    terminado Operaciones/Gine

--- OPERACIONESHOSPITALARIAS/reposicionVenta.php
+++ OPERACIONESHOSPITALARIAS/reposicionVenta.php
@@ -1,2 +1,12 @@
-<?php require("menuOperaciones.php"); ?>
-<?php require('/configuracion/clases/reposicionxVenta.php'); ?>
+<?php //require("menuOperaciones.php"); 
+require("/configuracion/ventanasEmergentes.php");
+require('/configuracion/funciones.php');
+
+$mostrarmenu=new menus();
+$mostrarmenu->menuTemplate($_GET['warehouse'],$_GET['datawarehouse'],$rutasalir,$rutapasswd,$usuario,$entidad,$rutamenuprincipal,'principal',$rutaimagen,$basedatos);
+
+?>
+<?php require('/configuracion/clases/reposicionxVenta.php'); 
+$mostrarFooter=new menus();
+$mostrarFooter->footerTemplate();
+?>
