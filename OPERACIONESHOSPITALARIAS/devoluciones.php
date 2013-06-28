9c90db3c3301f580b8ae40caa8bc50b67721ec94
EDER MARTINEZ <edermtz.04@gmail.com>
2013-06-28 11:55:22 -0500

    terminado Operaciones/Varios

--- OPERACIONESHOSPITALARIAS/devoluciones.php
+++ OPERACIONESHOSPITALARIAS/devoluciones.php
@@ -1,4 +1,15 @@
-<?php require("menuOperaciones.php");
-require('/configuracion/clases/devolucionesInternos.php');?>
+<?php //require("menuOperaciones.php");
+require("/configuracion/ventanasEmergentes.php");
+require('/configuracion/funciones.php');
+
+$mostrarmenu=new menus();
+$mostrarmenu->menuTemplate($_GET['warehouse'],$_GET['datawarehouse'],$rutasalir,$rutapasswd,$usuario,$entidad,$rutamenuprincipal,'principal',$rutaimagen,$basedatos);
+
+
+require('/configuracion/clases/devolucionesInternos.php');
+
+$mostrarFooter=new menus();
+$mostrarFooter->footerTemplate();
+?>
 
 
