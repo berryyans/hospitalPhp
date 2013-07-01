0a37f3ae81800083a5be86696f2b6e42b7bfbf4d
EDER MARTINEZ <edermtz.04@gmail.com>
2013-07-01 16:57:20 -0500

    terminado menus completos

--- OPERACIONESHOSPITALARIAS/subirImagen.php
+++ OPERACIONESHOSPITALARIAS/subirImagen.php
@@ -1,7 +1,16 @@
-<?php require("menuOperaciones.php");  ?>
+<?php //require("menuOperaciones.php");  
+require("/configuracion/ventanasEmergentes.php");
+require('/configuracion/funciones.php');
+
+$mostrarmenu=new menus();
+$mostrarmenu->menuTemplate($_GET['warehouse'],$_GET['datawarehouse'],$rutasalir,$rutapasswd,$usuario,$entidad,$rutamenuprincipal,'principal',$rutaimagen,$basedatos);
+
+?>
 <?php require("/configuracion/clases/editarResultados.php"); ?>
 <?php  
 $reporteReportes='agregarImagen.php';
 $editarResultados=new  editarResultados();
 $editarResultados->editaResultados($entidad,$reporteReportes,$fecha1,$ventana,$TITULO,$_GET['datawarehouse'],$basedatos);
+$mostrarFooter=new menus();
+$mostrarFooter->footerTemplate();
 ?>
