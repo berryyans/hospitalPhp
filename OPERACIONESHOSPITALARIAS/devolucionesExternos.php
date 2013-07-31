a12f710c75085c242789ce1a73b13109699d37ce
EDER MARTINEZ <edermtz.04@gmail.com>
2013-07-31 18:02:57 -0500

    commit pantallas

--- OPERACIONESHOSPITALARIAS/devolucionesExternos.php
+++ OPERACIONESHOSPITALARIAS/devolucionesExternos.php
@@ -1,12 +1,12 @@
 <?php //require("menuOperaciones.php");
-require("/configuracion/ventanasEmergentes.php");
-require('/configuracion/funciones.php');
+require("../configuracion/ventanasEmergentes.php");
+require('../configuracion/funciones.php');
 
 $mostrarmenu=new menus();
 $mostrarmenu->menuTemplate($_GET['warehouse'],$_GET['datawarehouse'],$rutasalir,$rutapasswd,$usuario,$entidad,$rutamenuprincipal,'principal',$rutaimagen,$basedatos);
 
 
-require('/configuracion/clases/traeFV.php');
+require('../configuracion/clases/traeFV.php');
 $tF=new traerFolios();
 $tF->foliosDevolucion($entidad,$basedatos);
 
--- OPERACIONESHOSPITALARIAS/presupuesto.php
+++ OPERACIONESHOSPITALARIAS/presupuesto.php
@@ -1,12 +1,12 @@
 <?php //require("../OPERACIONESHOSPITALARIAS/menuOperaciones.php"); 
-require("/configuracion/ventanasEmergentes.php");
-require('/configuracion/funciones.php');
+require("../configuracion/ventanasEmergentes.php");
+require('../configuracion/funciones.php');
 
 $mostrarmenu=new menus();
 $mostrarmenu->menuTemplate($_GET['warehouse'],$_GET['datawarehouse'],$rutasalir,$rutapasswd,$usuario,$entidad,$rutamenuprincipal,'principal',$rutaimagen,$basedatos);
 
 ?>
-<?php include("/configuracion/clases/presupuestoPaciente.php"); 
+<?php include("../configuracion/clases/presupuestoPaciente.php"); 
 $mostrarFooter = new menus();
 $mostrarFooter->footerTemplate($usuario,$entidad,$basedatos);
 ?>
